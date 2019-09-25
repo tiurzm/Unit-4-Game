427da36b31ab371e85db9a4f537984999490c178
Tiur Mawaddah <tiurzm@gmail.com>
2019-09-25 11:33:04 -0500

    <COMMENT>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,41 @@
 $(document).ready(function(){
     var targetScore = "";
-    for(var i = 0; i < 1; i++) {
-        var randomScore = Math.round(Math.random()*120);
-        targetScore = randomScore;
-        $("#target-score").text(targetScore);
-        console.log(targetScore);
-    }
+    var randomScore = Math.round(Math.random()*120)+1;
+    targetScore = randomScore;
+    console.log(typeof targetScore);
+    $("#target-score").text(targetScore);
+    // console.log(targetScore);
 
-    var numArray = [4, 5, 6, 7];
-    for(var j = 0; j < numArray.length; j++){
-        var crystal = $("<img>");
-        crystal.addClass("image");
-        crystal.attr("src", "./assets/images/1.jpg");
-        crystal.attr("data-value", numArray[j]);
-        $("#pictures").append(crystal);
-    };
-    $(".image").first().attr("src", "./assets/images/2.jpg");
-    $(".image").last().attr("src", "./assets/images/3.jpg");
+    for(var i = 0; i < 4; i++){
+        var numArray = "";
+        var randomNumber= Math.round(Math.random()*11)+1;
+        numArray = randomNumber; 
+        console.log(typeof numArray);
+        $(".vl").attr("data-value", numArray);
+    }; 
+
+    
 
 
-    var counter = 0;
-    var winScore = [];
-    var losseScore = [];
-    $(".image").on("click", function(){
-        var value = ($(this).attr("data-value"));
-        value = parseInt(value);
-        counter = counter + value;
-        if(counter === targetScore){
-            alert("you win");
-            winScore++; 
-            $("#wins").text(winScore);
-        } else if (counter > targetScore) {
-            alert("You lose!!");
-            console.log(losseScore);
-            losseScore++;
-            $("#losses").text(losseScore);
-        }
-        $("#total-score").text(counter);
-    });
+    // var counter = 0;
+    // var winScore = [];
+    // var losseScore = [];
+    // $(".image").on("click", function(){
+    //     var value = ($(this).attr("data-value"));
+    //     value = parseInt(value);
+    //     counter = counter + value;
+    //     if(counter === targetScore){
+    //         alert("you win");
+    //         winScore++; 
+    //         $("#wins").text(winScore);
+    //     } else if (counter > targetScore) {
+    //         alert("You lose!!");
+    //         console.log(losseScore);
+    //         losseScore++;
+    //         $("#losses").text(losseScore);
+    //     }
+    //     $("#total-score").text(counter);
+    // });
 
 
 });
